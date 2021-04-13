5fde45cd78a1740948785f943e5519bf24ca6538
Austin <aus343@gmail.com>
2021-04-13 10:20:43 -0500

    Less Intrusive Version Shower

--- LevelImposter-BepInEx/Harmony/Patches/VersionPatch.cs
+++ LevelImposter-BepInEx/Harmony/Patches/VersionPatch.cs
@@ -2,15 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace LevelImposter.Harmony.Patches
 {
-    [HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
+    [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
     public static class VersionPatch
     {
-        public static void Postfix(PingTracker __instance)
+        public static void Postfix(VersionShower __instance)
         {
-            __instance.text.text += "\n<color=#3399FF>Level<color=#FF0000>Imposter<color=#FFFFFF>\nv" + MainHarmony.VERSION + "\nby DigiWorm";
+            __instance.text.text += "\n<color=#3399FF>Level<color=#FF0000>Imposter<color=#FFFFFF> v" + MainHarmony.VERSION;
+            __instance.text.transform.position += new Vector3(0, -0.15f, 0);
         }
     }
 }
