0a232a9aca93c9bf06e31482f2f2aa1e2040d6b5
Digi <aus343@gmail.com>
2022-07-23 23:50:56 -0500

    Minimap Bug Fixes

--- LevelImposter/Core/Builders/MinimapBuilder.cs
+++ LevelImposter/Core/Builders/MinimapBuilder.cs
@@ -7,19 +7,22 @@ namespace LevelImposter.Core
 {
     public class MinimapBuilder : Builder
     {
+
+        private bool isBuilt = false;
+
         public void Build(LIElement elem, GameObject obj)
         {
             if (elem.type != "util-minimap")
                 return;
+            if (isBuilt)
+            {
+                LILogger.Warn("Only 1 util-minimap should be used per map");
+                return;
+            }
 
             ShipStatus shipStatus = LIShipStatus.Instance.shipStatus;
-            MapBehaviour mapBehaviour = MapBehaviour.Instance;
+            MapBehaviour mapBehaviour = GetMinimap();
 
-            if (mapBehaviour == null)
-            {
-                mapBehaviour = UnityEngine.Object.Instantiate(shipStatus.MapPrefab, HudManager.Instance.transform);
-                mapBehaviour.gameObject.SetActive(false);
-            }
 
             SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
             if (spriteRenderer != null)
@@ -27,22 +30,41 @@ namespace LevelImposter.Core
                 Sprite sprite = spriteRenderer.sprite;
                 GameObject background = mapBehaviour.ColorControl.gameObject;
                 background.GetComponent<SpriteRenderer>().sprite = sprite;
-                background.transform.localScale = obj.transform.localScale;
+                background.transform.localScale = obj.transform.localScale * 0.2f;
                 background.transform.localRotation = obj.transform.localRotation;
             }
 
             Transform hereIndicatorParent = mapBehaviour.transform.FindChild("HereIndicatorParent");
-            hereIndicatorParent.localPosition = new Vector3(0, 5.0f, -0.1f);
+            hereIndicatorParent.localPosition = new Vector3(0, 5.0f, -0.1f) - (obj.transform.localPosition * 0.2f);
 
             Transform roomNames = mapBehaviour.transform.FindChild("RoomNames");
             roomNames.gameObject.SetActive(false);
 
             obj.SetActive(false);
+            isBuilt = true;
         }
 
         public void PostBuild()
         {
+            if (!isBuilt)
+            {
+                MapBehaviour mapBehaviour = GetMinimap();
+                mapBehaviour.ColorControl.gameObject.SetActive(false);
+                mapBehaviour.transform.FindChild("HereIndicatorParent").gameObject.SetActive(false);
+                mapBehaviour.transform.FindChild("RoomNames").gameObject.SetActive(false);
+            }
+            isBuilt = false;
+        }
 
+        private MapBehaviour GetMinimap()
+        {
+            MapBehaviour mapBehaviour = MapBehaviour.Instance;
+            if (mapBehaviour == null)
+            {
+                mapBehaviour = UnityEngine.Object.Instantiate(LIShipStatus.Instance.shipStatus.MapPrefab, HudManager.Instance.transform);
+                mapBehaviour.gameObject.SetActive(false);
+            }
+            return mapBehaviour;
         }
     }
 }
