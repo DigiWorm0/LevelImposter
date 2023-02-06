4bd780f89249439c91ff05a9041bac4d62904fb9
Digi <aus343@gmail.com>
2023-02-06 16:02:35 -0600

    Added Fallback Spawn Point

--- LevelImposter/Core/Builders/Util/SpawnBuilder.cs
+++ LevelImposter/Core/Builders/Util/SpawnBuilder.cs
@@ -7,6 +7,10 @@ namespace LevelImposter.Core
 {
     public class SpawnBuilder : IElemBuilder
     {
+        private bool _hasInitialSpawn = false;
+        private bool _hasMeetingSpawn = false;
+        private Vector2 _fallbackSpawn = new Vector2();
+
         public void Build(LIElement elem, GameObject obj)
         {
             if (!elem.type.StartsWith("util-spawn"))
@@ -14,18 +18,23 @@ namespace LevelImposter.Core
             if (LIShipStatus.Instance?.ShipStatus == null)
                 throw new Exception("ShipStatus not found");
 
-            // Set Spawn Point
-            Vector2 pos = obj.transform.position - new Vector3(0f, 0.3636f, 0f);
+            // Ship Status
             ShipStatus shipStatus = LIShipStatus.Instance.ShipStatus;
             shipStatus.SpawnRadius = 1.55f;
+
+            // Set Spawn Point
+            Vector2 pos = obj.transform.position - new Vector3(0f, 0.3636f, 0f);
+            _fallbackSpawn = pos;
             if (elem.type == "util-spawn1")
             {
                 shipStatus.InitialSpawnCenter = pos;
+                _hasInitialSpawn = true;
             }
-            else if (elem.type == "util-spawn2")
+            if (elem.type == "util-spawn2")
             {
                 shipStatus.MeetingSpawnCenter = pos;
                 shipStatus.MeetingSpawnCenter2 = pos;
+                _hasMeetingSpawn = true;
             }
             else
             {
@@ -33,6 +42,21 @@ namespace LevelImposter.Core
             }
         }
 
-        public void PostBuild() { }
+        public void PostBuild()
+        {
+            if (LIShipStatus.Instance?.ShipStatus == null)
+                throw new Exception("ShipStatus not found");
+            ShipStatus shipStatus = LIShipStatus.Instance.ShipStatus;
+
+            if (!_hasMeetingSpawn)
+            {
+                shipStatus.MeetingSpawnCenter = _fallbackSpawn;
+                shipStatus.MeetingSpawnCenter2 = _fallbackSpawn;
+            }
+            if (!_hasInitialSpawn)
+            {
+                shipStatus.InitialSpawnCenter = _fallbackSpawn;
+            }
+        }
     }
 }
