d3aa657715f76e15ca5f9e1cc28d08d9fc04f3fd
gendelo3 <gendelo3@gmail.com>
2023-02-07 16:35:32 +0100

    Add TOR compatibility to LI, basically applying the TOU_Miner patch to Trickster JackInTheBox-Vents

--- LevelImposter/Core/Utils/ModCompatibility.cs
+++ LevelImposter/Core/Utils/ModCompatibility.cs
@@ -14,12 +14,15 @@ namespace LevelImposter.Core
         public const string TOU_GUID = "com.slushiegoose.townofus";
         public const string SUBMERGED_GUID = "Submerged";
         public const ShipStatus.MapType SUBMERGED_MAP_TYPE = (ShipStatus.MapType)5;
+        public const string TOR_GUID = "me.eisbison.theotherroles";
 
         private static bool _isTOUEnabled = false;
         private static bool _isSubmergedEnabled = false;
+        private static bool _isTOREnabled = false;
 
         public static bool IsTOUEnabled => _isTOUEnabled;
         public static bool IsSubmergedEnabled => _isSubmergedEnabled;
+        public static bool IsTOREnabled => _isTOREnabled;
 
         public static void Init()
         {
@@ -30,6 +33,10 @@ namespace LevelImposter.Core
             _isSubmergedEnabled = IL2CPPChainloader.Instance.Plugins.TryGetValue(SUBMERGED_GUID, out PluginInfo _);
             if (_isSubmergedEnabled)
                 LILogger.Info("LevelImposter detected Submerged installed, currently unsupported");
+
+            _isTOREnabled = IL2CPPChainloader.Instance.Plugins.TryGetValue(TOR_GUID, out PluginInfo _);
+            if (_isTOREnabled)
+                LILogger.Info("LevelImposter detected TOR installed, compatibility enabled");
         }
     }
 }
