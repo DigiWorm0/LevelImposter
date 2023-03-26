dde288985e41fe640a57594b960294f2385ef1d3
Digi <aus343@gmail.com>
2023-03-26 00:36:04 -0500

    Refactored Task Builder Classes

---
+++ LevelImposter/Core/Builders/Task/TaskBuilder.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.Events;
+using UnityEngine;
+using LevelImposter.DB;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+
+namespace LevelImposter.Core
+{
+    public class TaskBuilder : IElemBuilder
+    {
+        private TaskConsoleBuilder _consoleBuilder = new();
+        private ShipTaskBuilder _shipBuilder = new();
+
+        public void Build(LIElement elem, GameObject obj)
+        {
+            if (!elem.type.StartsWith("task-"))
+                return;
+
+            // Prefab 
+            var prefab = AssetDB.GetObject(elem.type);
+            if (prefab == null)
+                return;
+            
+            // Sprite
+            SpriteRenderer spriteRenderer = MapUtils.CloneSprite(obj, prefab);
+
+            // Console
+            Console console = _consoleBuilder.Build(elem, obj, prefab);
+            _shipBuilder.Build(elem, console);
+
+            // Button
+            var prefabBtn = prefab.GetComponentInChildren<PassiveButton>();
+            MapUtils.CreateDefaultColliders(obj, prefab);
+            if (prefabBtn != null)
+            {
+                PassiveButton btn = obj.AddComponent<PassiveButton>();
+                btn.ClickMask = obj.GetComponent<Collider2D>();
+                btn.OnMouseOver = new UnityEvent();
+                btn.OnMouseOut = new UnityEvent();
+                Action action = console.Use;
+                btn.OnClick.AddListener(action);
+            }
+
+            // Medscan
+            bool isMedscan = elem.type == "task-medscan";
+            if (isMedscan)
+            {
+                ShipStatus? shipStatus = LIShipStatus.Instance?.ShipStatus;
+                if (shipStatus == null)
+                    throw new Exception("Missing ShipStatus");
+                if (shipStatus.MedScanner != null)
+                    LILogger.Warn("Only 1 med scanner can be used per map");
+                MedScannerBehaviour medscan = obj.AddComponent<MedScannerBehaviour>();
+                shipStatus.MedScanner = medscan;
+            }
+        }
+
+        public void PostBuild()
+        {
+            _consoleBuilder.PostBuild();
+            _shipBuilder.PostBuild();
+        }
+    }
+}
