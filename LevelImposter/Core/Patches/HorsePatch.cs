7241141918ab3820a3a7726d2e3cb89cd4bc118e
Digi <aus343@gmail.com>
2023-03-31 19:56:42 -0500

    Hot-Fix for Horses

---
+++ LevelImposter/Core/Patches/HorsePatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using LevelImposter.Shop;
+using PowerTools;
+
+namespace LevelImposter.Core
+{
+    /*
+     *      Disables horse mod
+     */
+    [HarmonyPatch(typeof(Constants), nameof(Constants.ShouldHorseAround))]
+    public static class HorsePatch
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
