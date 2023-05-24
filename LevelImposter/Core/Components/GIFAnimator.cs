aa8cde23a340efd6d1db94b4ac3647497c6a0685
Digi <aus343@gmail.com>
2023-05-23 19:34:05 -0500

    Added Cloneable GIFAnimator

--- LevelImposter/Core/Components/GIFAnimator.cs
+++ LevelImposter/Core/Components/GIFAnimator.cs
@@ -3,8 +3,10 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BepInEx.Unity.IL2CPP;
 using BepInEx.Unity.IL2CPP.Utils.Collections;
 using Il2CppInterop.Runtime.Attributes;
+using Il2CppInterop.Runtime.InteropTypes.Fields;
 
 namespace LevelImposter.Core
 {
@@ -17,6 +19,8 @@ namespace LevelImposter.Core
         {
         }
 
+        private static Dictionary<long, GIFAnimator> _allAnimators = new();
+        private static long _nextAnimatorID = 1;
         private static readonly List<string> AUTOPLAY_BLACKLIST = new()
         {
             "util-vent1",
@@ -26,14 +30,16 @@ namespace LevelImposter.Core
             "util-cam"
         };
 
+        public Il2CppValueField<long> AnimatorID;
+        public bool IsAnimating => _isAnimating;
+
         private bool _defaultLoopGIF = false;
         private bool _isAnimating = false;
         private float[]? _delays;
         private Sprite[]? _frames;
         private SpriteRenderer? _spriteRenderer;
         private Coroutine? _animationCoroutine = null;
-
-        public bool IsAnimating => _isAnimating;
+        private long _animatorID => AnimatorID.Get();
 
         /// <summary>
         /// Initializes the component with GIF data
@@ -114,18 +120,37 @@ namespace LevelImposter.Core
         }
 
         /// <summary>
-        /// Copies animation data from another GIFAnimator
+        /// Fires when the animator is cloned
         /// </summary>
-        /// <param name="component">GIFAnimator to copy data from</param>
-        public void CopyFrom(GIFAnimator component)
+        /// <param name="original"></param>
+        public void OnClone(GIFAnimator originalAnim)
         {
             _spriteRenderer = GetComponent<SpriteRenderer>();
-            _frames = component._frames;
-            _delays = component._delays;
+            _frames = originalAnim._frames;
+            _delays = originalAnim._delays;
+            _defaultLoopGIF = originalAnim._defaultLoopGIF;
         }
 
+        public void Awake()
+        {
+            // Check if object was cloned
+            if (_animatorID != 0)
+            {
+                bool objectExists = _allAnimators.TryGetValue(_animatorID, out var originalObject);
+                if (objectExists && originalObject != null)
+                {
+                    OnClone(originalObject);
+                }
+            }
+
+            // Update Object ID
+            AnimatorID.Set(_nextAnimatorID++);
+            _allAnimators.Add(_animatorID, this);
+        }
         public void OnDestroy()
         {
+            _allAnimators.Remove(_animatorID);
+
             _delays = null;
             _frames = null;
             _spriteRenderer = null;
