7a4c3d59b7063a453c8de61036b842dc1f1bcb23
ykundesu <49261069+ykundesu@users.noreply.github.com>
2023-08-31 00:28:58 +0900

    Fixed an issue where RPCs matched vanilla CancelPet. (#34)
    Co-authored-by: Digi <aus343@gmail.com>

--- LevelImposter/Core/Models/LIRpc.cs
+++ LevelImposter/Core/Models/LIRpc.cs
@@ -1,17 +1,17 @@
 ﻿namespace LevelImposter.Core
 {
-    // Among Us     0 - 32
+    // Among Us     0 - 50
     // TOR          60 - 73, 100 - 149
     // Las Monjas   60 - 69, 75 - 194
     // StellaRoles  60 - 169
     // ToU          100 - 210, 220 - 251
     // Submerged    210 - 214
 
-    // LI           50 - 59 (Guess I'll exist here...)
+    // LI           51 - 59 (Guess I'll exist here...)
 
     public enum LIRpc
     {
-        FireTrigger = 50,   // Fires a global trigger on an object
+        FireTrigger = 51,   // Fires a global trigger on an object
         TeleportPlayer,     // Uses a util-tele object
         SyncMapID,          // Syncs the map ID in the lobby
         SyncRandomSeed,     // Syncs a random seed for util-triggerrand
