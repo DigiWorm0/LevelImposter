0d7cecd24c5b98d669b272877d8e16d3852bd717
Digi <digiworm0@gmail.com>
2024-09-22 15:10:01 -0500

    Improved Lobby Tag

--- LevelImposter/Shop/Components/LobbyVersionTag.cs
+++ LevelImposter/Shop/Components/LobbyVersionTag.cs
@@ -22,13 +22,13 @@ public class LobbyVersionTag(IntPtr intPtr) : MonoBehaviour(intPtr)
     {
         // Create Textbox
         _text = gameObject.AddComponent<TextMeshPro>();
-        _text.fontSize = 1.4f;
+        _text.fontSize = 1.6f;
 
         // Position Textbox
         var position = new Vector3
         {
             x = 4.9f,
-            y = -5.1f,
+            y = -5.05f,
             z = -30
         };
         transform.localPosition = position;
@@ -64,6 +64,7 @@ public class LobbyVersionTag(IntPtr intPtr) : MonoBehaviour(intPtr)
         StringBuilder versionTagBuilder = new();
 
         // Map Name
+        versionTagBuilder.Append("<font=\"Barlow-Black SDF\" material=\"Barlow-Black Outline\">");
         if (isFallback || currentMap == null)
         {
             versionTagBuilder.Append("Random Custom Map\n");
@@ -71,17 +72,22 @@ public class LobbyVersionTag(IntPtr intPtr) : MonoBehaviour(intPtr)
         else
         {
             versionTagBuilder.Append($"<color=#1a95d8>{currentMap.name}</color>");
-            versionTagBuilder.Append($" by {currentMap.authorName}\n");
+            versionTagBuilder.Append($" by {currentMap.authorName}");
         }
 
+        versionTagBuilder.Append("</font>");
+
         // Version
-        versionTagBuilder.Append("<size=1.3>");
-        versionTagBuilder.Append("<color=#1a95d888>Level</color>");
-        versionTagBuilder.Append("<color=#cb282888>Imposter</color>");
+        versionTagBuilder.Append('\n');
+        versionTagBuilder.Append("<font=\"VCR SDF\">");
+        versionTagBuilder.Append("<size=1.2>");
+        versionTagBuilder.Append("<color=#1a95d888>L</color>");
+        versionTagBuilder.Append("<color=#cb282888>I</color>");
         versionTagBuilder.Append("<color=#ffffff88> v");
         versionTagBuilder.Append(LevelImposter.DisplayVersion);
         versionTagBuilder.Append("</color>");
         versionTagBuilder.Append("</size>");
+        versionTagBuilder.Append("</font>");
 
         // Set Text
         Instance._text.text = versionTagBuilder.ToString();
