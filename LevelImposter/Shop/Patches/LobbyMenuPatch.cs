65f586a7d0602ed22893a91f5ecfcf2a54c7eaa7
Digi <digiworm0@gmail.com>
2024-09-22 15:27:03 -0500

    "Fix" Custom Maps on New Lobbies

--- LevelImposter/Shop/Patches/LobbyMenuPatch.cs
+++ LevelImposter/Shop/Patches/LobbyMenuPatch.cs
@@ -1,84 +1,25 @@
-﻿using HarmonyLib;
+﻿using AmongUs.GameOptions;
+using HarmonyLib;
 using LevelImposter.Core;
 
 namespace LevelImposter.Shop;
+
 /*
- *      Replace Map Name in Options Console.
- *      Also skips Submerged, if it isn't present.
+ *      Resets the map to Skeld when creating a new lobby
  */
-/*
-[HarmonyPatch(typeof(KeyValueOption))]
-public static class MapNameValuePatch
+[HarmonyPatch(typeof(MMOnlineManager), nameof(MMOnlineManager.Start))]
+public static class LastMapValuePatch
 {
-    /// <summary>
-    /// Removes all placeholder or unavailable maps
-    /// </summary>
-    /// <param name="__instance">KeyValueOption Instance</param>
-    private static void RemoveElems(KeyValueOption __instance)
+    public static void Postfix()
     {
-        bool isMapLoaded = MapLoader.CurrentMap != null;
-        for (int i = __instance.Values.Count - 1; i >= 0; i--)
-        {
-            string mapName = __instance.Values[i].Key;
-            bool isPlaceholder = string.IsNullOrEmpty(mapName);
-            bool isUnavailable = !isMapLoaded && mapName == LIConstants.MAP_NAME;
-            if (isPlaceholder || isUnavailable)
-            {
-                __instance.Values.RemoveAt(i);
-                if (__instance.Selected >= __instance.Values.Count)
-                    __instance.Selected--;
-            }
-        }
-    }
+        var options = GameOptionsManager.Instance.CurrentGameOptions;
+        if (options.MapId != (byte)MapType.LevelImposter)
+            return;
 
-    [HarmonyPrefix]
-    [HarmonyPatch(nameof(KeyValueOption.FixedUpdate))]
-    public static bool UpdateFix(KeyValueOption __instance)
-    {
-        bool isMapTitle = __instance.Title == StringNames.GameMapName;
-        bool hasChanged = __instance.oldValue != __instance.Selected;
-        bool shouldShowName = __instance.Selected == (int)MapType.LevelImposter || !MapLoader.IsFallback;
-        bool isMapLoaded = MapLoader.CurrentMap != null;
-
-        if (isMapTitle && hasChanged)
-        {
-            RemoveElems(__instance);
-            if (shouldShowName && isMapLoaded)
-            {
-                __instance.oldValue = __instance.Selected;
-
-
-                if (MapLoader.IsFallback) // Random Map
-                    __instance.ValueText.text = LIConstants.MAP_NAME;
-                else // Map Name
-                    __instance.ValueText.text = $"{MapLoader.CurrentMap?.name}\n<size=0.9>by {MapLoader.CurrentMap?.authorName}";
-
-                return false;
-            }
-        }
-        return true;
-    }
-
-    [HarmonyPrefix]
-    [HarmonyPatch(nameof(KeyValueOption.Increase))]
-    [HarmonyPatch(nameof(KeyValueOption.Decrease))]
-    public static void IncrementFix(KeyValueOption __instance)
-    {
-        if (__instance.Title == StringNames.GameMapName)
-        {
-            RemoveElems(__instance);
-            ConfigAPI.SetLastMapID(null);
-
-            if (!MapLoader.IsFallback || MapLoader.CurrentMap == null)
-                MapSync.RegenerateFallbackID();
-            else
-                MapSync.SyncMapID();
-        }
+        GameOptionsManager.Instance.CurrentGameOptions.SetByte(ByteOptionNames.MapId, (byte)MapType.Skeld);
+        GameOptionsManager.Instance.GameHostOptions = GameOptionsManager.Instance.CurrentGameOptions;
     }
 }
-*/
-
-// TODO: Fix Lobby Map Selection
 
 /*
  *      Initializes a new Map Console in the Lobby
@@ -89,20 +30,5 @@ public static class LobbyMenuInitPatch
     public static void Postfix()
     {
         LobbyConsoleBuilder.Build();
-    }
-}
-
-/*
- *      Replaces the LI map name with the actual map name
- */
-[HarmonyPatch(typeof(IGameOptionsExtensions), nameof(IGameOptionsExtensions.ToHudString))]
-public static class StringRenamePatch
-{
-    public static void Postfix(ref string __result)
-    {
-        if (MapLoader.CurrentMap == null || MapLoader.IsFallback)
-            return;
-
-        __result = __result.Replace(LIConstants.MAP_NAME, MapLoader.CurrentMap.name);
     }
 }
