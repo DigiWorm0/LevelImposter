27ae05102b6013a86e892d4e04aabb4cff1ed3d5
Digi <digiworm0@gmail.com>
2024-09-22 20:06:45 -0500

    Fix Custom Text String Parameters

--- LevelImposter/Core/Patches/Utils/RenamePatch.cs
+++ LevelImposter/Core/Patches/Utils/RenamePatch.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using Il2CppSystem;
 using ObjList = Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<Il2CppSystem.Object>;
 
 namespace LevelImposter.Core;
@@ -43,7 +44,7 @@ public static class TaskRenamePatch
 public static class StringRenamePatch
 {
     public static bool Prefix([HarmonyArgument(0)] StringNames stringNames,
-        [HarmonyArgument(1)] ObjList _, // TODO: Format parameters into string
+        [HarmonyArgument(1)] ObjList objList,
         ref string __result)
     {
         // Handle Special Cases
@@ -57,7 +58,13 @@ public static class StringRenamePatch
         if (!LIShipStatus.IsInstance() || !LIShipStatus.GetInstance().Renames.Contains(stringNames))
             return true;
 
+        // Get the string from the database
         __result = LIShipStatus.GetInstance().Renames.Get(stringNames);
+
+        // Format parameters into string
+        // MUST BE Il2cppSystem.String.Format (not System.String.Format)
+        __result = String.Format(__result, objList);
+
         return false;
     }
 }
