7fe56e244525150a5eece4c19011b7284e1d67ab
Digi <digiworm0@gmail.com>
2025-12-04 23:57:46 -0600

    Fixed Lobby Loading

--- LevelImposter/Core/Models/LIConstants.cs
+++ LevelImposter/Core/Models/LIConstants.cs
@@ -8,7 +8,7 @@ public static class LIConstants
     public const StringNames MAP_STRING_NAME = (StringNames)392001; // StringName that placeholdes the map names
     public const string MAP_NAME = "Random Custom Map"; // Name to populate Constants.MapNames
     public const float PLAYER_POS = -5.0f; // Z value of the player
-    public const int MAX_LOAD_TIME = 10; // Maximum time to build map before aborting
+    public const int MAX_LOAD_TIME = 9; // Maximum time to build map before aborting
     public const int MAX_CONNECTION_TIMEOUT = 10; // Maximum time to wait for a client connection
 
     public const int
--- LevelImposter/Core/Patches/Loading/LoadingShipPatch.cs
+++ LevelImposter/Core/Patches/Loading/LoadingShipPatch.cs
@@ -11,20 +11,18 @@ public static class LoadingShipPatch
 {
     public static bool Prefix(InnerNetClient __instance)
     {
-        // // Continue if not in a game
-        // if (!LIShipStatus.IsInstance())
-        //     return true;
-        //
-        // // Continue if ship is already loaded
-        // if (LIShipStatus.GetInstance().IsReady)
-        //     return true;
-        //
-        // // Wait for ship to finish loading, then send packet
-        // MapUtils.WaitForShip(LIConstants.MAX_LOAD_TIME, __instance.SendClientReady);
-        //
-        // // Don't send packet
-        // return false;
-        // TODO: FIX ME
-        return true;
+        // Continue if not in a game
+        if (!LIShipStatus.IsInstance())
+            return true;
+
+        // Continue if ship is already loaded
+        if (LIShipStatus.GetInstance().IsReady)
+            return true;
+        
+        // Wait for ship to finish loading, then send packet
+        MapUtils.WaitForShip(LIConstants.MAX_LOAD_TIME, __instance.SendClientReady);
+        
+        // Don't send packet
+        return false;
     }
 }
