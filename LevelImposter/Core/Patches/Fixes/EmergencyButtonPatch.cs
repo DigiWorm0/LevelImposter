418fc2488e077da96aad849c4c104b1283d3e766
PalmForest <realpalmforest@gmail.com>
2026-08-13 21:39:03 +1000

    Patch BreakEmergencyButton with custom LI logic

---
+++ LevelImposter/Core/Patches/Fixes/EmergencyButtonPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using LevelImposter.Core.Components;
+using LevelImposter.Core.Utils;
+
+namespace LevelImposter.Core.Patches.Fixes;
+
+/// <summary>
+///     Patches the Emergency Button breaking logic to destroy all buttons on the map.
+/// </summary>
+[HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.BreakEmergencyButton))]
+public static class EmergencyButtonPatch
+{
+    public static bool Prefix(ShipStatus __instance)
+    {
+        if (!LIShipStatus.IsInstance())
+            return true;
+
+        var ship = LIShipStatus.GetInstance();
+
+        foreach (var button in ship.transform.GetComponentsInChildren<SystemConsole>())
+        {
+            button.Image.sprite = __instance.BrokenEmergencyButton;
+            button.enabled = false;
+        }
+
+        LILogger.Msg("Hooked into ShipStatus.BreakEmergencyButton() and broke all emergency buttons.");
+
+        return false;
+    }
+}
